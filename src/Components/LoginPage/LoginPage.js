6b6914c4a7f769527809735b47c8c4c94f2c8c5c
kzv <yg9699k@naver.com>
2021-04-16 13:12:08 +0900

    second commit

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -2,16 +2,22 @@ import React from 'react'
 import '../../style/LoginPage.scss'
 
 function LoginPage() {
+
+    const onSubmitHandler = e => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className="loginComp">
-            <h1>로그인</h1>
-            <form>
+            {/* <h1>로그인</h1> */}
+            <form onSubmit={onSubmitHandler}>
 
                 <div className="formBox">
                     <label htmlFor="id">아이디</label>
                     <div className="inputBox">
                         <input type="text" name="id" />
-                        <img src={process.env.PUBLIC_URL + '/icons/fail_icon.svg'} />
+                        {/* <img src={process.env.PUBLIC_URL + '/icons/fail_icon.svg'} /> */}
                     </div>
 
 
@@ -23,6 +29,8 @@ function LoginPage() {
                         <input type="password" name="pwd" />
                     </div>
                 </div>
+
+                <button type="submit">로그인</button>
 
             </form>
         </div>
