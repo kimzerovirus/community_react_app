35f88021fe0f7b53e04b54304e132aa08556028b
kimzerovirus <yg9699k@naver.com>
2022-02-06 23:38:40 +0900

    로그인페이지 스타일링

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -17,72 +17,51 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="2xl:container h-screen m-auto">
-			<div hidden className="fixed inset-0 w-6/12 lg:block">
-				<img
-					src={`${path}/전구맨2.gif`}
-					className="w-full h-full object-cover"
-					alt="로그인이미지"
-				/>
-			</div>
-			<div className="relative h-full ml-auto lg:w-6/12 flex items-center">
-				<div className="m-auto py-12 px-6 sm:p-20 xl:w-10/12">
-					<div className="space-y-4 text-center font-extrabold text-3xl">
-						<Link to="">THE CONNECTER</Link>
-						<p className="font-medium text-lg text-gray-600 text-center">
-							Welcome to job web!
-						</p>
-					</div>
-
-					<form onSubmit={submitHandler} className="space-y-6 py-6">
-						<div className="relative">
-							<input
-								type="email"
-								name="email"
-								placeholder="Email"
-								className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
-							/>
-						</div>
-
-						<div className="flex flex-col items-end">
-							<input
-								type="password"
-								name="password"
-								placeholder="Password"
-								className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
-							/>
-							<Link to="/find" className="w-max p-3 -mr-3">
-								<span className="text-sm tracking-wide text-blue-600">
-									Forgot password
-								</span>
-							</Link>
-						</div>
-
-						<div>
-							<button
-								type="submit"
-								className="w-full px-6 py-3 rounded-xl bg-sky-500 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800"
-							>
-								<span className="font-semibold text-white text-lg">Login</span>
-							</button>
-							<Link to="/signup" className="w-max p-3 -ml-3">
-								<span className="text-sm tracking-wide text-blue-600">
-									Create new account
-								</span>
-							</Link>
-						</div>
-					</form>
-
-					<div className="border-t pt-12">
-						<div className="space-y-2 text-center">
-							<img src="" alt="" className="w-40 m-auto grayscale" />
-							<span className="block text-sm tracking-wide text-gray-400">
-								Copyright &copy; 2021 KZV&KJY ALL Right reserved.
-							</span>
-						</div>
-					</div>
+		<div className="flex items-center justify-center h-screen bg-gray-100">
+			<form
+				className="w-72 lg:w-112 lg:p-6 lg:bg-white lg:shadow-md lg:rounded-lg"
+				onSubmit={submitHandler}
+			>
+				<div>
+					<h1 className="text-2xl">로그인</h1>
 				</div>
-			</div>
+				<div className="mt-4">
+					<input
+						type="email"
+						name="email"
+						id=""
+						placeholder="이메일 주소"
+						className="w-full px-4 py-3 rounded-lg mt-2 border focus:border-blue-600 focus:bg-white focus:outline-none "
+						required
+					/>
+				</div>
+
+				<div className="mt-4">
+					<input
+						type="password"
+						name="password"
+						id=""
+						placeholder="비밀번호"
+						className="w-full px-4 py-3 rounded-lg mt-2 border focus:border-blue-600
+                  focus:bg-white focus:outline-none"
+						required
+					/>
+				</div>
+
+				<div className="text-right mt-2">
+					<Link to="/signup" className="text-sm font-semibold text-blue-700">
+						회원가입
+					</Link>
+				</div>
+
+				<button
+					type="submit"
+					className="w-full block bg-blue-600 hover:bg-blue-900 focus:bg-blue-900 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6"
+				>
+					로그인
+				</button>
+			</form>
 		</div>
 	);
 };
