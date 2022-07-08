6ff9e6716e0a451650868a80d1a50540eeb586ae
kimzerovirus <yg9699k@naver.com>
2022-07-09 01:55:56 +0900

    markdown file reading system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,73 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-const Home: NextPage = () => {
+interface PostsProps {
+	posts: postProps[];
+}
+
+interface postProps {
+	slug: string;
+	frontmatter: {
+		title: string;
+		date: string;
+		except?: string;
+		cover_image?: string;
+	};
+}
+
+export default function Home({ posts }: PostsProps) {
 	return (
 		<div>
 			<Head>
 				<title>kimzerovirus.log</title>
 			</Head>
 
-			<h2>Hello</h2>
+			<div className="posts">
+				{posts.map((post, index) => (
+					<div key={index}>
+						<h3>{post.frontmatter.title}</h3>
+						<img
+							src={
+								post.frontmatter.cover_image
+									? post.frontmatter.cover_image
+									: post.frontmatter.cover_image
+							}
+							alt="cover_image"
+						/>
+						<p>{post.frontmatter.date}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
-};
+}
 
-export default Home;
+export async function getStaticProps() {
+	const files = fs.readdirSync(path.join('posts'));
+
+	const posts = files.map(filename => {
+		const slug = filename.replace('.md', '');
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts', filename),
+			'utf-8',
+		);
+
+		const { data: frontmatter } = matter(markdownWithMeta);
+
+		return {
+			slug,
+			frontmatter,
+		};
+	});
+
+	console.log(posts);
+
+	return {
+		props: {
+			posts,
+		},
+	};
+}
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+title: 'Test post'
+date: '2022-07-09'
+except: 'This is except'
+cover_image: 'images/test.jpg'
+---
+
+## Heading
+- head
