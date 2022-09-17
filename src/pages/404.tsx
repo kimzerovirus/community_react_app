3f2dc73b58205114dc943029d786d8c5ae5abfe4
kimzerovirus <yg9699k@naver.com>
2022-09-17 19:43:12 +0900

    NOT FOUND - 404error 추가

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+// import LaunchSharpIcon from '@mui/icons-material/LaunchSharp';
+// import OpenInNewSharpIcon from '@mui/icons-material/OpenInNewSharp';
+// import HomeSharpIcon from '@mui/icons-material/HomeSharp';
+import { Container, Grid, Link, Typography } from '@mui/material';
+import Lottie from 'react-lottie-player';
+
+import lottieJson from '../../public/cat.json';
+
+export default function PageNotFound() {
+	return (
+		<Container component="main" maxWidth="xs" style={{ marginTop: '10%' }}>
+			<Lottie loop animationData={lottieJson} play style={{ width: `100%`, height: `100%` }} />
+
+			<Grid container justifyContent="center" mt={2}>
+				<Grid item>
+					<Typography component="h1" variant="h5">
+						해당 페이지를 찾지 못했습니다.
+					</Typography>
+				</Grid>
+				<Grid item>
+					<Link href="/" variant="body2">
+						메인페이지로 이동
+						{/* <HomeSharpIcon sx={{ fontSize: '0.875rem' }} /> */}
+					</Link>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+}
