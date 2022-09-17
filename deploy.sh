72c54ed932e96ba1585d1dd6a5472e953d8d4296
kimzerovirus <yg9699k@naver.com>
2022-09-17 20:30:22 +0900

    message

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ message=""
 
 if ["$1"=""]
 then message="New Deployment"
-else message=$1
+else message=$*
 fi
 
 git add -A
-git commit -m  $1
+git commit -m message
 git push origin main
 
 npm run deploy
