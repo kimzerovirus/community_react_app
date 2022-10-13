df6dd3da438e8a86690427046933b31ec15860d4
kimzerovirus <yg9699k@naver.com>
2022-10-14 00:14:43 +0900

    페이징 처리 추가

--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -18,13 +18,13 @@ type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<S
 // 	htmlstring: string;
 // }
 
-export const useInput = <T>(initialData: T): ReturnTypes<T> => {
-	const [value, setValue] = useState(initialData);
-	const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-		setValue(e.target.value as T);
-	}, []);
-	return [value, handler, setValue];
-};
+// export const useInput = <T>(initialData: T): ReturnTypes<T> => {
+// 	const [value, setValue] = useState(initialData);
+// 	const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+// 		setValue(e.target.value as unknown as T);
+// 	}, []);
+// 	return [value, handler, setValue];
+// };
 
 export const usePreventCopy = () => {
 	useEffect(() => {
